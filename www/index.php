4b8cbf76e376d21aab8e205d440075171b57f7f3
Leandro Arturi <lea.arturi@gmail.com>
2022-06-02 09:50:18 -0300

    Retornar la cantidad de elementos con edad mayor o igual que 50

--- www/index.php
+++ www/index.php
@@ -1,6 +1,3 @@
-<p>
-    <a href="lab/fibonacci.php">Fibonacci</a>
-</p>
-<p>
-    <a href="lab/equipos-peligrosos.php">Equipos Peligrosos</a>
-</p>
+<p><a href="lab/fibonacci.php">Fibonacci</a></p>
+<p><a href="lab/equipos-peligrosos.php">Equipos Peligrosos</a></p>
+<p><a href="lab/count-elements.php">Contar cantidad elementos API Array</a></p>
